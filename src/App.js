da068a321450c27ac745aa8c9d65478d4ef21e58
pleportz <pleportz@gmail.com>
2019-06-08 23:10:43 +0200

    First experiment with css grid

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import { ColorBlock } from "./components";
 
 function App() {
-  const blocks = new Array(30).fill(1);
+  const blocks = new Array(20).fill(1);
   return (
     <div className="App">
+      <h1 className="header">We love plants</h1>
       {blocks.map((_, index) => {
         return <ColorBlock key={index} />;
       })}

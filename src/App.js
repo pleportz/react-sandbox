ca4e8f6baa42497883907e45d8341d06bb5f5c11
pleportz <pleportz@gmail.com>
2019-06-09 01:26:38 +0200

    Nest blocks grid within App grid

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ function App() {
   const blocks = new Array(20).fill(1);
   return (
     <div className="App">
-      <h1 className="header">We love plants</h1>
-      {blocks.map((_, index) => {
-        return <ColorBlock key={index} />;
-      })}
+      <h1>
+        We love plants{" "}
+        <span role="img" aria-label="plant">
+          🌱
+        </span>
+      </h1>
+      <div className="color-blocks">
+        {blocks.map((_, index) => {
+          return <ColorBlock key={index} />;
+        })}
+      </div>
     </div>
   );
 }

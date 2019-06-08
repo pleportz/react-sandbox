838b4e153f80c06c3b1db3b725b3f4ef9a99625e
pleportz <pleportz@gmail.com>
2019-06-08 23:46:46 +0200

    Display plant photo on hover

--- src/components/ColorBlock/ColorBlock.js
+++ src/components/ColorBlock/ColorBlock.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./ColorBlock.css";
+import lemonTreesTwoMonthsOld from "../../images/lemonTreesTwoMonthsOld.webp";
 
 const COLORS = ["red", "blue", "yellow"];
 
 export const ColorBlock = _ => {
   const backgroundColor = COLORS[Math.floor(Math.random() * 3)];
-  return <div className={`color-block color-block--${backgroundColor}`} />;
+  return (
+    <div className={`color-block--${backgroundColor}`}>
+      <img src={lemonTreesTwoMonthsOld} alt="plant" />
+    </div>
+  );
 };

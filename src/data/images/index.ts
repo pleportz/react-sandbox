ef82aa271ea25406ad6ab439963cccebe077ebab
pleportz <pleportz@gmail.com>
2019-06-09 19:41:05 +0200

    Optimize images with webp format at 90% quality

--- src/data/images/index.ts
+++ src/data/images/index.ts
@@ -1,11 +1,11 @@
-import avocado_and_medlar_trees_20180409 from "./avocado_and_medlar_trees_20180409.jpeg";
-import gotcha from "./gotcha.jpeg";
-import lemon_trees_20180510 from "./lemon_trees_20180510.jpeg";
-import lemon_trees_20180512 from "./lemon_trees_20180512.jpeg";
-import lemon_trees_20180515 from "./lemon_trees_20180515.jpeg";
-import lemon_trees_20180529 from "./lemon_trees_20180529.jpeg";
-import lemon_trees_20180604 from "./lemon_trees_20180604.jpeg";
-import lemon_trees_20180710 from "./lemon_trees_20180710.jpeg";
+import avocado_and_medlar_trees_20180409 from "./avocado_and_medlar_trees_20180409.webp";
+import gotcha from "./gotcha.webp";
+import lemon_trees_20180510 from "./lemon_trees_20180510.webp";
+import lemon_trees_20180512 from "./lemon_trees_20180512.webp";
+import lemon_trees_20180515 from "./lemon_trees_20180515.webp";
+import lemon_trees_20180529 from "./lemon_trees_20180529.webp";
+import lemon_trees_20180604 from "./lemon_trees_20180604.webp";
+import lemon_trees_20180710 from "./lemon_trees_20180710.webp";
 
 export default {
   lemon_trees_20180510,

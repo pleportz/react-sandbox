bdd1776d9c4e3919c4ccfdb99fdc65f6a4bab6f0
pleportz <pleportz@gmail.com>
2019-06-09 19:28:15 +0200

    Add typescript to project

--- src/components/ColorBlock/ColorBlock.tsx
+++ src/components/ColorBlock/ColorBlock.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./ColorBlock.css";
+import { Plant } from "../../data/plants";
+
+interface Props {
+  plant: Plant;
+}
 
 const COLORS = ["red", "blue", "yellow"];
 
-export const ColorBlock = ({ plant }) => {
+export const ColorBlock = ({ plant }: Props) => {
   const backgroundColor = COLORS[Math.floor(Math.random() * 3)];
   const { name, image } = plant;
   return (
--- src/data/plants.ts
+++ src/data/plants.ts
@@ -3,7 +3,19 @@ import images from "./images";
 const LEMON_TREES = "Lemon trees";
 const AVOCADO_AND_MEDLAR_TREES = "Avocado and medlar trees";
 
-const getPlant = (name, imageSource, imageDate) => ({
+export interface Plant {
+  name: string;
+  image: {
+    source: string;
+    date: Date;
+  };
+}
+
+const getPlant = (
+  name: string,
+  imageSource: string,
+  imageDate: Date
+): Plant => ({
   name,
   image: {
     source: imageSource,
@@ -11,7 +23,9 @@ const getPlant = (name, imageSource, imageDate) => ({
   }
 });
 
-export const plants = [
+console.log(images.lemon_trees_20180510);
+
+export const plants: Plant[] = [
   getPlant(LEMON_TREES, images.lemon_trees_20180510, new Date("2018-05-10")),
   getPlant(LEMON_TREES, images.lemon_trees_20180512, new Date("2018-05-12")),
   getPlant(LEMON_TREES, images.lemon_trees_20180515, new Date("2018-05-15")),
